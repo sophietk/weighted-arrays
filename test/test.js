34318eb0712b31baaa8b6c237e7e6a5705362feb
sophietk <sophie.tk@gmail.com>
2018-11-24 11:49:16 +0100

    use modern es syntax

--- test/test.js
+++ test/test.js
@@ -1,54 +1,47 @@
 const { describe, it } = require('mocha')
-var wa = require('../lib/weighted-arrays')
 
-var identity = function (value) {
-  return value
-}
-var sameWeight = function () {
-  return 1
-}
-var isValue = function (expected) {
-  return function (value) {
-    return value === expected
-  }
-}
+const wa = require('../lib/weighted-arrays')
 
-describe('random', function () {
-  it('should return only value with weight', function () {
-    var array = [0, 1, 0]
-    var random = wa.random(array, identity)
+const identity = value => value
+const sameWeight = () => 1
+const isValue = expected => value => value === expected
+
+describe('random', () => {
+  it('should return only value with weight', () => {
+    const array = [0, 1, 0]
+    const random = wa.random(array, identity)
 
     random.should.equal(1)
   })
 
-  it('should not return a 0-weighted value', function () {
-    var array = [0, 1, 2]
-    var random = wa.random(array, identity)
+  it('should not return a 0-weighted value', () => {
+    const array = [0, 1, 2]
+    const random = wa.random(array, identity)
 
     random.should.not.equal(0)
   })
 
-  it('should return one of values with weight', function () {
-    var array = [0, 1, 2]
-    var random = wa.random(array, identity);
+  it('should return one of values with weight', () => {
+    const array = [0, 1, 2]
+    const random = wa.random(array, identity);
 
     [1, 2].should.containEql(random)
   })
 
-  it('should works with non-integer weights', function () {
-    var array = [0, 0.33, 0.44]
-    var random = wa.random(array, identity)
+  it('should works with non-integer weights', () => {
+    const array = [0, 0.33, 0.44]
+    const random = wa.random(array, identity)
 
     random.should.not.equal(0);
     [0.33, 0.44].should.containEql(random)
   })
 
-  it('should return random value coherent with weight', function () {
-    var array = [0, 1, 3, 7]
-    var draws = 10000
-    var delta = draws * 0.1
-    var randoms = []
-    for (var i = 0; i < draws; i++) {
+  it('should return random value coherent with weight', () => {
+    const array = [0, 1, 3, 7]
+    const draws = 10000
+    const delta = draws * 0.1
+    const randoms = []
+    for (let i = 0; i < draws; i++) {
       randoms.push(wa.random(array, identity))
     }
 
@@ -60,19 +53,19 @@ describe('random', function () {
   })
 })
 
-describe('probability', function () {
-  it('should return 0 for a 0-weighted value', function () {
-    var array = [0, 1, 2]
+describe('probability', () => {
+  it('should return 0 for a 0-weighted value', () => {
+    const array = [0, 1, 2]
 
     wa.probability(array, identity, 0).should.equal(0)
   })
 
-  it('should return same probability for same weight values', function () {
-    var array = [0, 1, 2, 3]
-    var p0 = wa.probability(array, sameWeight, 0)
-    var p1 = wa.probability(array, sameWeight, 1)
-    var p2 = wa.probability(array, sameWeight, 2)
-    var p3 = wa.probability(array, sameWeight, 3)
+  it('should return same probability for same weight values', () => {
+    const array = [0, 1, 2, 3]
+    const p0 = wa.probability(array, sameWeight, 0)
+    const p1 = wa.probability(array, sameWeight, 1)
+    const p2 = wa.probability(array, sameWeight, 2)
+    const p3 = wa.probability(array, sameWeight, 3)
 
     p0.should.equal(1 / 4)
     p1.should.equal(1 / 4)
@@ -80,12 +73,12 @@ describe('probability', function () {
     p3.should.equal(1 / 4)
   })
 
-  it('should return coherent probability', function () {
-    var array = [0, 1, 2, 3]
-    var p0 = wa.probability(array, identity, 0)
-    var p1 = wa.probability(array, identity, 1)
-    var p2 = wa.probability(array, identity, 2)
-    var p3 = wa.probability(array, identity, 3)
+  it('should return coherent probability', () => {
+    const array = [0, 1, 2, 3]
+    const p0 = wa.probability(array, identity, 0)
+    const p1 = wa.probability(array, identity, 1)
+    const p2 = wa.probability(array, identity, 2)
+    const p3 = wa.probability(array, identity, 3)
 
     p0.should.equal(0)
     p1.should.equal(1 / 6)
